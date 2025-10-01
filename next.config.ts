d60a018d439d6ac156fd69713bf2d272df1b33c1
PARANDHAMAREDDYBOMMAKA <rparandhama63@gmail.com>
2025-10-01 13:57:08 +0530

    fixed auth issue

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     '/api/**': ['./node_modules/**'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ignore warnings from Prisma instrumentation and OpenTelemetry
+      config.ignoreWarnings = [
+        { module: /node_modules\/@prisma\/instrumentation/ },
+        { module: /node_modules\/@opentelemetry\/instrumentation/ },
+      ]
+    }
+    return config
+  },
 };
 
 export default nextConfig;
